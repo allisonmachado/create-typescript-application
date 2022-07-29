7299d5c113de1a456a9b9f1896ca7b4b63e9580a
Allison Machado Gonçalves <aallison93@gmail.com>
2022-07-29 18:31:24 +0100

    feat: bootstrap application from template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+#! /usr/bin/env node
+const prompt = require('prompt');
+const shell = require('shelljs');
+
+const properties = [
+  {
+    name: 'app-name',
+    validator: /^[a-z-]+$/,
+    warning: 'App name must be only lower case letters or dashes',
+  },
+];
+
+prompt.start();
+
+prompt.get(properties, (err, result) => {
+  if (err) {
+    console.log(err);
+    return 1;
+  }
+
+  shell.exec(`curl -L https://github.com/allisonmachado/typescript-nodejs-template/archive/refs/heads/master.tar.gz > ${result['app-name']}.tar.gz`);
+  shell.exec(`tar -xzf ${result['app-name']}.tar.gz`);
+  shell.exec(`mv typescript-nodejs-template-master ${result['app-name']}`);
+  shell.exec(`rm ${result['app-name']}.tar.gz`);
+
+  return undefined;
+});
