55692b8a172717814db2bed705c392654dc4aee2
Allison Machado <aallison93@gmail.com>
2022-08-02 18:02:43 +0100

    feat: stablish application logging (#22)
    * chore: improve error message
    
    * feat: logging steps of execution
    
    * fix: hide curl output
    
    * chore: improve log messages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const prompt = require('prompt');
 const shell = require('shelljs');
+const winston = require('winston');
 
 const templateDownloadUrl = 'https://github.com/allisonmachado/typescript-nodejs-template/archive/refs/heads/master.tar.gz';
 
@@ -8,9 +9,15 @@ const properties = [
   {
     name: 'app-name',
     validator: /^[a-z-]+$/,
-    warning: 'App name must be only lower case letters or dashes',
+    warning: 'App name must only contain lower case letters and dashes, e.g: "my-app"',
   },
 ];
+
+const logger = winston.createLogger({
+  level: 'silly',
+  format: winston.format.cli(),
+  transports: [new winston.transports.Console()],
+});
 
 const hasCurlInstalled = () => !!shell.which('curl');
 const hasWgetInstalled = () => !!shell.which('wget');
@@ -20,16 +27,28 @@ prompt.start();
 prompt.get(properties, (_error, result) => {
   const chosenAppName = result['app-name'];
 
+  logger.info(`chosen app name: ${chosenAppName}`);
+
   if (hasWgetInstalled()) {
-    shell.exec(`wget -qO- ${templateDownloadUrl} > ${chosenAppName}.tar.gz`);
+    const command = `wget -qO- ${templateDownloadUrl} > ${chosenAppName}.tar.gz`;
+    logger.debug(`downloading template: ${command}`);
+
+    shell.exec(command);
   } else if (hasCurlInstalled()) {
-    shell.exec(`curl -L ${templateDownloadUrl} > ${chosenAppName}.tar.gz`);
+    const command = `curl -s -L ${templateDownloadUrl} > ${chosenAppName}.tar.gz`;
+    logger.debug(`downloading template: ${command}`);
+
+    shell.exec(command);
   } else {
-    throw new Error('Sorry, this script requires that either curl or wget is installed in the system');
+    logger.error('Sorry, this script requires that either curl or wget is installed in the system');
+    process.exit(1);
   }
+
   shell.exec(`tar -xzf ${chosenAppName}.tar.gz`);
   shell.mv('typescript-nodejs-template-master', `${chosenAppName}`);
   shell.rm(`${chosenAppName}.tar.gz`);
+
+  logger.info(`app ${chosenAppName} initialized successfully!`);
 
   return undefined;
 });
