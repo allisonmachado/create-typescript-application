5b3c69354f0e4637a68de4807cffba4b946f61ce
Allison Machado Gonçalves <aallison93@gmail.com>
2022-08-02 17:09:46 +0100

    feat: logging steps of execution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const prompt = require('prompt');
 const shell = require('shelljs');
+const winston = require('winston');
 
 const templateDownloadUrl = 'https://github.com/allisonmachado/typescript-nodejs-template/archive/refs/heads/master.tar.gz';
 
@@ -12,6 +13,12 @@ const properties = [
   },
 ];
 
+const logger = winston.createLogger({
+  level: 'silly',
+  format: winston.format.cli(),
+  transports: [new winston.transports.Console()],
+});
+
 const hasCurlInstalled = () => !!shell.which('curl');
 const hasWgetInstalled = () => !!shell.which('wget');
 
@@ -20,16 +27,24 @@ prompt.start();
 prompt.get(properties, (_error, result) => {
   const chosenAppName = result['app-name'];
 
+  logger.info(`chosen app name: ${chosenAppName}`);
+
   if (hasWgetInstalled()) {
+    logger.debug('downloading via wget');
     shell.exec(`wget -qO- ${templateDownloadUrl} > ${chosenAppName}.tar.gz`);
   } else if (hasCurlInstalled()) {
+    logger.debug('downloading via curl');
     shell.exec(`curl -L ${templateDownloadUrl} > ${chosenAppName}.tar.gz`);
   } else {
-    throw new Error('Sorry, this script requires that either curl or wget is installed in the system');
+    logger.error('Sorry, this script requires that either curl or wget is installed in the system');
+    process.exit(1);
   }
+
   shell.exec(`tar -xzf ${chosenAppName}.tar.gz`);
   shell.mv('typescript-nodejs-template-master', `${chosenAppName}`);
   shell.rm(`${chosenAppName}.tar.gz`);
+
+  logger.info(`app ${chosenAppName} initialized successfully!`);
 
   return undefined;
 });
